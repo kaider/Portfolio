eb5519da9d5e11bd8d018416af7079abb200e7a4
kaider <leonxdinh@gmail.com>
2022-05-29 00:10:05 -0400

    Finished AboutPage

--- js/scripts.js
+++ js/scripts.js
@@ -54,48 +54,52 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-    // Fancy box
-    Fancybox.bind('[data-fancybox="gallery"]', {
-        animated: false,
-        showClass: false,
-        hideClass: false,
-      
-        dragToClose: false,
-      
-        closeButton: "top",
-      
-        Thumbs: false,
-        Toolbar: false,
-      
-        Carousel: {
-          // Enable dots
-          Dots: true,
-      
-          // Disable touch guestures
-          Panzoom: {
-            touch: false,
-          },
-      
-          // Disable sliding animation
-          friction: 0,
-        },
-      
-        Image: {
-          zoom: false,
-          click: false,
-          wheel: false,
-          fit: "contain-w",
-        },
-      
-        on: {
-          init: (fancybox) => (fancybox.prevScrollTop = 0),
-          done: (fancybox, slide) => (slide.$el.scrollTop = fancybox.prevScrollTop),
-          "Carousel.change": (fancybox, carousel, to, from) => {
-            fancybox.prevScrollTop = carousel.slides[from].$el.scrollTop || 0;
-      
-            if (carousel.slides[to].$el) {
-              carousel.slides[to].$el.scrollTop = fancybox.prevScrollTop;
-            }
-          },
-        },
-      });
+// Copyright year
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+// Fancy box
+Fancybox.bind('[data-fancybox="gallery"]', {
+    animated: false,
+    showClass: false,
+    hideClass: false,
+  
+    dragToClose: false,
+  
+    closeButton: "top",
+  
+    Thumbs: false,
+    Toolbar: false,
+  
+    Carousel: {
+      // Enable dots
+      Dots: true,
+  
+      // Disable touch guestures
+      Panzoom: {
+        touch: false,
+      },
+  
+      // Disable sliding animation
+      friction: 0,
+    },
+  
+    Image: {
+      zoom: false,
+      click: false,
+      wheel: false,
+      fit: "contain-w",
+    },
+  
+    on: {
+      init: (fancybox) => (fancybox.prevScrollTop = 0),
+      done: (fancybox, slide) => (slide.$el.scrollTop = fancybox.prevScrollTop),
+      "Carousel.change": (fancybox, carousel, to, from) => {
+        fancybox.prevScrollTop = carousel.slides[from].$el.scrollTop || 0;
+  
+        if (carousel.slides[to].$el) {
+          carousel.slides[to].$el.scrollTop = fancybox.prevScrollTop;
+        }
+      },
+    },
+  }
+);
