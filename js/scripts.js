6489859ae00e85680b69b43a0f6d939013030592
kaider <leonxdinh@gmail.com>
2022-05-31 23:59:51 -0400

    filling out main gallery contents

--- js/scripts.js
+++ js/scripts.js
@@ -59,47 +59,10 @@ document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
 // Fancy box
 Fancybox.bind('[data-fancybox="gallery"]', {
-    animated: false,
-    showClass: false,
-    hideClass: false,
-  
-    dragToClose: false,
-  
-    closeButton: "top",
-  
-    Thumbs: false,
-    Toolbar: false,
-  
+  Thumbs: {
     Carousel: {
-      // Enable dots
-      Dots: true,
-  
-      // Disable touch guestures
-      Panzoom: {
-        touch: false,
-      },
-  
-      // Disable sliding animation
-      friction: 0,
+      fill: false,
+      center: true,
     },
-  
-    Image: {
-      zoom: false,
-      click: false,
-      wheel: false,
-      fit: "contain-w",
-    },
-  
-    on: {
-      init: (fancybox) => (fancybox.prevScrollTop = 0),
-      done: (fancybox, slide) => (slide.$el.scrollTop = fancybox.prevScrollTop),
-      "Carousel.change": (fancybox, carousel, to, from) => {
-        fancybox.prevScrollTop = carousel.slides[from].$el.scrollTop || 0;
-  
-        if (carousel.slides[to].$el) {
-          carousel.slides[to].$el.scrollTop = fancybox.prevScrollTop;
-        }
-      },
-    },
-  }
-);
+  },
+});
